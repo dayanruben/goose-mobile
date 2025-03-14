9e12dd0dfd79ed3ae75523b9caa3508a6cf0a072
Douwe Osinga <douwe.osinga@gmail.com>
2025-03-14 14:02:42 -0700

    Set the accessibility (#75)
    Co-authored-by: Douwe Osinga <douwe@squareup.com>

--- sync_sessions.sh
+++ sync_sessions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while true; do
     adb pull /storage/emulated/0/Android/data/xyz.block.gosling/files/session_dumps/ .
+    adb shell settings put secure enabled_accessibility_services xyz.block.gosling/.features.accessibility.GoslingAccessibilityService
     sleep 1
 done
